a06f3efea3f54e84306cec8dd4059a3d690b410d
Dv1101 <dhruvvaria1101@gmail.com>
2024-02-16 01:26:41 +0530

    Update & Delete View and controller updated, _Shared Notification bootstrap Impolemented

--- DvInfoWeb/Controllers/CategoryController.cs
+++ DvInfoWeb/Controllers/CategoryController.cs
@@ -26,7 +26,7 @@ namespace DvInfoWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            if(obj.Name == obj.DisplayOrder.ToString())
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("Name", "Name and Display order shall be different !!");
             }
@@ -38,13 +38,100 @@ namespace DvInfoWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["SuccessMsg"] = "Category " + obj.Name + " Created.";
                 return RedirectToAction("Index", "Category");
             }
-            
+
             return View();
+
+
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.CategoryId == id);
+            Category?  categoryFromDb2 = _db.Categories.Where(u => u.CategoryId == id).FirstOrDefault();
+            if (categoryFromDb2 == null)
+            {
+                return NotFound();
+            } 
+
+            return View(categoryFromDb2);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            /*if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Name and Display order shall be different !!");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value");
+            }*/
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["SuccessMsg"] = "Category " + obj.Name + " Updated.";
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View();
+
+
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.CategoryId == id);
+            Category? categoryFromDb2 = _db.Categories.Where(u => u.CategoryId == id).FirstOrDefault();
+            if (categoryFromDb2 == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb2);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            /*if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Name and Display order shall be different !!");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value");
+            }*/
+            Category? obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["SuccessMsg"] = "Category " + obj.Name + " Deleted.";
+            return RedirectToAction("Index", "Category");
             
-            
-            
+
+            //return View();
+
+
+
         }
     }
 }
